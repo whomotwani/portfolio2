1addb0644b406c9d0a072a0dafb7919f18e163e1
Vijay Motwani <vijay.motwani@outlook.in>
2024-10-10 04:24:45 +0530

    Finished Portfolio2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,64 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <main class="container p-4 w-full md:w-3/4 mx-auto mb-28">
+    <About class="mt-12" />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    <section class="mt-12">
+      <p class="text-2xl mb-4">SKILLS</p>
+      <div class="badge badge-primary mr-2" v-for="skill in data.skills" :key="skill">{{ skill }}</div>
+    </section>
+
+    <section class="mt-12">
+      <p class="text-2xl mb-4">EXPERIENCES</p>
+      <div class="flex flex-col gap-3 mb-10" v-for="experience in data.experiences" :key="experience.name">
+        <div class="flex-1">
+          <p class="text-xl">{{ experience.name }}, {{ experience.location }}</p>
+          <p class="text-sm">{{ experience.startDate }} - {{ experience.endDate }}</p>
+        </div>
+        <div class="flex-1">
+          <p class="mb-2">{{ experience.description }}</p>
+          <div class="badge badge-primary badge-outline mr-2" v-for="skill in experience.skills" :key="skill">{{ skill
+            }}</div>
+        </div>
+      </div>
+
+      <div class="text-right">
+        <button class="btn btn-neutral" @click="openResume">
+          <span class="mr-1">View full resume</span>
+          <span class="material-symbols-outlined text-sm">
+            north_east
+          </span>
+        </button>
+      </div>
+    </section>
+
+    <section class="mt-12">
+      <p class="text-2xl mb-4">PROJECTS</p>
+      <div class="flex flex-col gap-3 mb-10" v-for="project in data.projects" :key="project.name">
+        <div class="flex-1">
+          <a :href="project.link" target="_blank" class="text-xl">
+            <span>
+              {{ project.name }}
+            </span>
+            <span class="material-symbols-outlined text-sm ml-1">
+              north_east
+            </span>
+          </a>
+          <p class="text-sm">{{ project.startDate }} - {{ project.endDate }}</p>
+        </div>
+        <div class="flex-1">
+          <p class="mb-2">{{ project.description }}</p>
+          <div class="badge badge-primary badge-outline mr-2" v-for="skill in project.skills" :key="skill">{{ skill }}
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<script setup>
+import data from './data.json'
+import About from './components/About.vue';
+function openResume() {
+  const pdfPath = `${import.meta.env.BASE_URL}Resume.pdf`; // Access the file from the public folder
+  window.open(pdfPath, '_blank'); // Opens the PDF in a new tab
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <h1 class="text-5xl">{{ data.name }}</h1>
+        <h3 class="text-2xl">{{ data.designation }}</h3>
+
+        <p class="text-xl mt-5">{{ data.comment }}</p>
+        <a class="btn btn-xs mr-2" v-for="i in data.social" :key="i.name" :href="i.url" target="_blank">
+            {{ i.name }}
+        </a>
+    </section>
+</template>
+<script setup>
+import data from '../data.json'
+</script>
