98e77700a1d5890eb2e9ff81c057a42b9900422e
Vijay Motwani <vijay.motwani@outlook.in>
2024-10-10 13:17:40 +0530

    Updated details + fixes styling issues

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="container p-4 w-full md:w-3/4 mx-auto mb-28">
-    <About class="mt-12" />
+  <main class="container p-4 w-full md:w-3/4 mx-auto mb-12 mt-12">
+    <About />
 
     <section class="mt-12">
       <p class="text-2xl mb-4">SKILLS</p>
@@ -11,19 +11,22 @@
       <p class="text-2xl mb-4">EXPERIENCES</p>
       <div class="flex flex-col gap-3 mb-10" v-for="experience in data.experiences" :key="experience.name">
         <div class="flex-1">
-          <p class="text-xl">{{ experience.name }}, {{ experience.location }}</p>
+          <p class="text-xl">
+            {{ experience.designation }} - {{ experience.name }}, {{ experience.location }}
+          </p>
           <p class="text-sm">{{ experience.startDate }} - {{ experience.endDate }}</p>
         </div>
         <div class="flex-1">
           <p class="mb-2">{{ experience.description }}</p>
-          <div class="badge badge-primary badge-outline mr-2" v-for="skill in experience.skills" :key="skill">{{ skill
-            }}</div>
+          <div class="badge badge-primary badge-outline mr-2" v-for="skill in experience.skills" :key="skill">
+            {{ skill }}
+          </div>
         </div>
       </div>
 
       <div class="text-right">
         <button class="btn btn-neutral" @click="openResume">
-          <span class="mr-1">View full resume</span>
+          <span class="mr-1">View resume</span>
           <span class="material-symbols-outlined text-sm">
             north_east
           </span>
